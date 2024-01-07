0ea58a433dcc489ec5b8031caab84ce6f88e6080
madhuroshan <madhuroshan2102@gmail.com>
2024-01-07 10:25:29 +0530

    asking question functionality

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,63 +5,13 @@ import NoResults from "@/components/shared/NoResults";
 import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
+import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 
-const questions = [
-  {
-    _id: "1",
-    title:
-      "How to use React Query? and How to start doing it?",
-    tags: [
-      {
-        _id: "1",
-        name: "React Query",
-      },
-      {
-        _id: "2",
-        name: "React",
-      },
-      {
-        _id: "3",
-        name: "JavaScript",
-      },
-    ],
-    author: {
-      _id: 1,
-      name: "John Doe",
-      picture: "/assets/icons/avatar.svg",
-    },
-    upvotes: 1034544,
-    views: 10099,
-    answers: [],
-    createdAt: new Date("2021-09-20T12:30:00.000Z"),
-  },
-  {
-    _id: "2",
-    title: "How to center a div?",
-    tags: [
-      {
-        _id: "1",
-        name: "Css",
-      },
-      {
-        _id: "2",
-        name: "flex",
-      },
-    ],
-    author: {
-      _id: 2,
-      name: "Jonas",
-      picture: "/assets/icons/avatar.svg",
-    },
-    upvotes: 14,
-    views: 200,
-    answers: [],
-    createdAt: new Date("2021-09-20T12:30:00.000Z"),
-  },
-];
+export default async function Home() {
+  const result = await getQuestions({});
 
-export default function Home() {
+  // console.log(result.questions);
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -94,8 +44,8 @@ export default function Home() {
 
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
