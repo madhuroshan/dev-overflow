c6b24be9f452cbefd9859a5ae8a59a042c98fe86
madhuroshan <madhuroshan2102@gmail.com>
2024-01-20 15:59:45 +0530

    add em all

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,13 +2,85 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetUserByIdParams,
+  UpdateUserParams,
+} from "./shares.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
-export async function getUserById(params: any) {
+export async function getUserById(
+  params: GetUserByIdParams
+) {
   try {
     connectToDatabase();
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function createUser(
+  userData: CreateUserParams
+) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate(
+      {
+        clerkId,
+      },
+      updateData,
+      {
+        new: true,
+      }
+    );
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Return the deleted user
+    // delete all his data
+
+    const userQuestionIds = await Question.find({
+      author: user._id,
+    }).distinct("_id");
+
+    await Question.deleteMany({ author: user._id });
+
+    // delete answers comments and many more
+    const deletedUser = await User.findByIdAndDelete(
+      user._id
+    );
+    return deletedUser;
   } catch (error) {
     console.log(error);
   }
