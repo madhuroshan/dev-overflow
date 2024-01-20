5f33a39b97abbb06410ae8f08355750b71fab6db
madhuroshan <madhuroshan2102@gmail.com>
2024-01-20 16:46:03 +0530

    completed setting webhooks

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -62,7 +62,7 @@ const QuestionCard = ({
 
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          imgUrl="/assets/icons/avatar.svg"
+          imgUrl={author.picture}
           value={author.name}
           title={`| asked ${getTimeStamp(createdAt)}`}
           alt="upvote"
